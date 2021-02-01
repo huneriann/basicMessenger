d658023fe52075cc2dfa7e4c462b7dc8edc7ab42
Ilkin Hashimzade <ilkin.hashimzada@gmail.com>
2021-02-02 00:58:22 +0400

    First version of BasicMessenger
    Added desktop version of Client and console version of server

---
+++ WebSocket/WebSocket.DesktopClient/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using WebSocket.DesktopClient.Service;
+
+namespace WebSocket.DesktopClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            
+        }
+
+    }
+}
